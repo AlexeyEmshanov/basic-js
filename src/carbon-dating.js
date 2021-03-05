014d1ba6c8b3f4485c1bea344abea5d25f95a890
AlexeyEmshanov <alexey.emshanov@gmail.com>
2021-03-05 19:39:04 +0400

    fix: adjust solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,10 +4,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (!isNaN(sampleActivity)) {
-    const years = - Math.log( sampleActivity / MODERN_ACTIVITY ) * HALF_LIFE_PERIOD / Math.LN2
-    return Math.ceil(years)
-  } else {
+  if ((typeof sampleActivity !== "string" ) || (isNaN(sampleActivity)) ) {
     return false  
+    
+  } else {
+    const years = - Math.log( sampleActivity / MODERN_ACTIVITY ) * HALF_LIFE_PERIOD / Math.LN2
+    return ((Math.ceil(years) !== Infinity) && (Math.ceil(years) > 0)) ? Math.ceil(years) : false
   }
 };
